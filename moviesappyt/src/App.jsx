66aa36c44c519e8036eb3335c20f34f53ad93e9f
Felipe <mufdi@tuta.io>
2022-06-18 04:33:44 -0400

    Movies app Part 1

--- moviesappyt/src/App.jsx
+++ moviesappyt/src/App.jsx
@@ -1,3 +1,15 @@
+import { MoviesGrid } from './MoviesGrid'
+import styles from './App.module.css'
+
 export function App () {
-  return <div> App </div>
+  return (
+    <div>
+      <header>
+       <h1 className={styles.title}> Movies </h1>
+      </header>
+      <main>
+        <MoviesGrid />
+      </main>
+    </div>
+  )
 }
---
+++ moviesappyt/src/MoviesGrid.jsx
@@ -0,0 +1,14 @@
+import movies from './movies.json'
+import { MovieCard } from './MovieCard'
+import styles from './MovieGrid.module.css'
+
+
+export function MoviesGrid () {
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map(movie =>
+        <MovieCard key={movie.id} movie={movie}/>
+      )}
+    </ul>
+  )
+}
