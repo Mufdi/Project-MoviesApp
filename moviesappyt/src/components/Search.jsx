3cedb0043cbde5ff57cf552213b6ee15c198f351
Felipe <mufdi@tuta.io>
2022-10-04 20:06:51 -0300

    Debounce search

--- moviesappyt/src/components/Search.jsx
+++ moviesappyt/src/components/Search.jsx
@@ -1,42 +1,32 @@
-import styles from './Search.module.css'
-import { CgSearch } from 'react-icons/cg';
-import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router'
-import { useQuery } from '../hooks/useQuery.jsx'
+import styles from "./Search.module.css";
+import { CgSearch } from "react-icons/cg";
+import { useHistory } from "react-router";
+import { useQuery } from "../hooks/useQuery.jsx";
 
+export function Search() {
+    const query = useQuery();
+    const search = query.get("search");
 
-export function Search () {
-  const query = useQuery()
-  const search = query.get("search")
+    const history = useHistory();
 
-  const [searchText, setSearchText] = useState("")
-  const history = useHistory()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
 
-  useEffect(() => {
-    setSearchText(search || "")
-  }, [search])
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    history.push("/?search=" + searchText)
-
-  }
-
-  return (
-    <form className={styles.searchContainer} onSubmit={handleSubmit}>
-      <div className={styles.searchBox}>
-        <input
-          className={styles.searchInput}
-          type="text"
-          value={searchText}
-          onChange={(e)=>setSearchText(e.target.value)}
-        />
-        <button className={styles.searchButton} type="submit">
-          <CgSearch size={20} />
-        </button>
-      </div>
-
-    </form>
-  )
+    return (
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input
+                    className={styles.searchInput}
+                    type="text"
+                    value={search}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                        history.push("/?search=" + value);
+                    }}
+                />
+                    <CgSearch size={25} color="black" className={styles.searchButton} />
+            </div>
+        </form>
+    );
 }
